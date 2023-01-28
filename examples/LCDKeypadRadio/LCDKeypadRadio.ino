5884da1a577ed5c0e06d9027faacf1e716c7d002
Matthias Hertel <mathertel@hotmail.com>
2023-01-28 13:53:59 +0100

    LCDKeypadRadio example repaired

--- examples/LCDKeypadRadio/LCDKeypadRadio.ino
+++ examples/LCDKeypadRadio/LCDKeypadRadio.ino
@@ -1,6 +1,6 @@
-/// \file LCDKeypadRadio.ino 
+/// \file LCDKeypadRadio.ino
 /// \brief Radio implementation using the Serial communication.
-/// 
+///
 /// \author Matthias Hertel, http://www.mathertel.de
 /// \copyright Copyright (c) 2014 by Matthias Hertel.\n
 /// This work is licensed under a BSD style license.\n
@@ -15,12 +15,11 @@
 /// ------
 /// The necessary wiring of the various chips are described in the Testxxx example sketches.
 /// The boards have to be connected by using the following connections:
-/// 
+///
 /// Arduino port | SI4703 signal | RDA5807M signal
 /// :----------: | :-----------: | :-------------:
-///          GND | GND           | GND   
-///         3.3V | VCC           | -
-///           5V | -             | VCC
+///          GND | GND           | GND
+///         3.3V | VCC           | VCC
 ///           A5 | SCLK          | SCLK
 ///           A4 | SDIO          | SDIO
 ///           D2 | RST           | -
@@ -33,6 +32,7 @@
 /// * 05.08.2014 created.
 /// * 04.10.2014 working.
 /// * 22.03.2015 Copying to LCDKeypadRadio.
+/// * 23.01.2023 more robust, nicer LCD messages and cleanup.
 
 #include <LiquidCrystal.h>
 
@@ -42,14 +42,19 @@
 #include <rda5807M.h>
 #include <si4703.h>
 #include <si4705.h>
-#include <si4721.h>
+#include <SI47xx.h>
 #include <tea5767.h>
 
 #include <RDSParser.h>
 
 // The keys available on the keypad
 enum KEYSTATE {
-  KEYSTATE_NONE, KEYSTATE_SELECT, KEYSTATE_LEFT, KEYSTATE_UP, KEYSTATE_DOWN, KEYSTATE_RIGHT
+  KEYSTATE_NONE,
+  KEYSTATE_SELECT,
+  KEYSTATE_LEFT,
+  KEYSTATE_UP,
+  KEYSTATE_DOWN,
+  KEYSTATE_RIGHT
 } __attribute__((packed));
 
 // ----- forwards -----
@@ -59,31 +64,18 @@ KEYSTATE getLCDKeypadKey();
 
 
 // Define some stations available at your locations here:
-// 89.40 MHz as 8940
+// 89.30 MHz as 8930
+// Be sure to enter a strong station in preset[0];
 
 RADIO_FREQ preset[] = {
-  8770,
-  8810, // hr1
-  8820,
-  8850, // Bayern2
-  8890, // ???
-  8930, // * hr3
-  8980,
-  9180,
-  9220, 9350,
-  9440, // * hr1
-  9510, // - Antenne Frankfurt
-  9530,
-  9560, // Bayern 1
-  9680, 9880,
-  10020, // planet
-  10090, // ffh
-  10110, // SWR3
-  10030, 10260, 10380, 10400,
-  10500 // * FFH
+  8930,  // * hr3
+  9270,
+  9310,
+  9670,
+  10250
 };
 
-int    i_sidx=5;        ///< Start at Station with index=5
+unsigned int presetIndex = 0;  ///< Start at Station with index=5
 
 /// The radio object has to be defined by using the class corresponding to the used chip.
 /// by uncommenting the right radio object definition.
@@ -92,23 +84,15 @@ int    i_sidx=5;        ///< Start at Station with index=5
 // RDA5807M radio;    ///< Create an instance of a RDA5807 chip radio
 // SI4703   radio;    ///< Create an instance of a SI4703 chip radio.
 // SI4705   radio;    ///< Create an instance of a SI4705 chip radio.
-SI4721   radio;    ///< Create an instance of a SI4721 chip radio.
+SI47xx radio;  ///< Create an instance of a SI4721 chip radio.
 // TEA5767  radio;    ///< Create an instance of a TEA5767 chip radio.
 
 
 /// get a RDS parser
 RDSParser rds;
 
-
-/// State definition for this radio implementation.
-enum RADIO_STATE {
-  STATE_PARSECOMMAND, ///< waiting for a new command character.
-  
-  STATE_PARSEINT,     ///< waiting for digits for the parameter.
-  STATE_EXEC          ///< executing the command.
-};
-
-RADIO_STATE state; ///< The state variable is used for parsing input characters.
+unsigned long nextFreqTime = 0;
+unsigned long nextClearTime = 0;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -119,38 +103,37 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 /// This function will be when a new frequency is received.
 /// Update the Frequency on the LCD display.
-void DisplayFrequency(RADIO_FREQ f)
-{
+void DisplayFrequency() {
   char s[12];
   radio.formatFrequency(s, sizeof(s));
-  lcd.setCursor(0, 0); lcd.print(s);
-} // DisplayFrequency()
+  lcd.setCursor(0, 0);
+  lcd.print(s);
+}  // DisplayFrequency()
 
 
 /// This function will be called by the RDS module when a new ServiceName is available.
 /// Update the LCD to display the ServiceName in row 1 chars 0 to 7.
-void DisplayServiceName(char *name)
-{
+void DisplayServiceName(const char *name) {
   size_t len = strlen(name);
   lcd.setCursor(0, 1);
   lcd.print(name);
   while (len < 8) {
     lcd.print(' ');
-    len++;  
-  } // while
-} // DisplayServiceName()
+    len++;
+  }  // while
+}  // DisplayServiceName()
 
 
 /// This function will be called by the RDS module when a rds time message was received.
 /// Update the LCD to display the time in right upper corner.
 void DisplayTime(uint8_t hour, uint8_t minute) {
-  lcd.setCursor(11, 0); 
+  lcd.setCursor(11, 0);
   if (hour < 10) lcd.print('0');
   lcd.print(hour);
   lcd.print(':');
   if (minute < 10) lcd.print('0');
   lcd.println(minute);
-} // DisplayTime()
+}  // DisplayTime()
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -162,8 +145,12 @@ void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t blo
 
 
 /// This function determines the current pressed key but
-/// * doesn't return short key down situations that are key bouncings.
+/// * doesn't return short key down situations that are key signal bouncing.
 /// * returns a specific key down only once.
+///
+/// [Next] [seek--] [Vol+] [seek++] [RST]
+///              [Vol-]
+///
 KEYSTATE getLCDKeypadKey() {
   static unsigned long lastChange = 0;
   static KEYSTATE lastKey = KEYSTATE_NONE;
@@ -172,8 +159,8 @@ KEYSTATE getLCDKeypadKey() {
   KEYSTATE newKey;
 
   // Get current key state
-  int v = analogRead(A0); // the buttons are read from the analog0 pin
-  
+  int v = analogRead(A0);  // the buttons are read from the analog0 pin
+
   if (v < 100) {
     newKey = KEYSTATE_RIGHT;
   } else if (v < 200) {
@@ -197,23 +184,23 @@ KEYSTATE getLCDKeypadKey() {
   } else if (lastChange == 0) {
     // nothing new.
     return (KEYSTATE_NONE);
-
-  } if (now > lastChange + 50) {
+  }
+  if (now > lastChange + 50) {
     // now its not a bouncing button any more.
-    lastChange = 0; // don't report twice
+    lastChange = 0;  // don't report twice
     return (newKey);
 
   } else {
     return (KEYSTATE_NONE);
 
-  } // if
-} // getLCDKeypadKey()
+  }  // if
+}  // getLCDKeypadKey()
 
 
 /// Setup a FM only radio configuration with I/O for commands and debugging on the Serial port.
 void setup() {
   // open the Serial port
-  Serial.begin(57600);
+  Serial.begin(115200);
   Serial.println("LCDKeypadRadio...");
 
   // set up the LCD's number of columns and rows:
@@ -221,49 +208,51 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("LCDKeypadRadio");
-  delay(1000);
+  delay(2000);
   lcd.clear();
 
-  // Initialize the Radio 
-  radio.init();
+  // Initialize the Radio
+  bool found = radio.initWire(Wire);
+
+  if (!found) {
+    Serial.println("No Radio Chip found.");
+    Serial.println("Press reset...");
+
+    lcd.setCursor(0, 0);
+    lcd.print("No Radio Chip");
+    lcd.setCursor(0, 1);
+    lcd.print("Press reset...");
+
+    while (1) {};
+  }
 
   // Enable information to the Serial port
   radio.debugEnable();
 
-  radio.setBandFrequency(RADIO_BAND_FM, preset[i_sidx]); // 5. preset.
+  radio.setBandFrequency(RADIO_BAND_FM, preset[presetIndex]);  // first preset.
 
   // delay(100);
 
   radio.setMono(false);
   radio.setMute(false);
-  // radio._wireDebug();
-  radio.setVolume(8);
+  radio.setVolume(radio.getMaxVolume());
 
   Serial.write('>');
-  
-  state = STATE_PARSECOMMAND;
-  
+
   // setup the information chain for RDS data.
   radio.attachReceiveRDS(RDS_process);
   rds.attachServiceNameCallback(DisplayServiceName);
   rds.attachTimeCallback(DisplayTime);
-} // Setup
+}  // Setup
 
 
-/// Constantly check for serial input commands and trigger command execution.
+/// Constantly check for keypad  and trigger command execution.
 void loop() {
-  int newPos;
   unsigned long now = millis();
-  static unsigned long nextFreqTime = 0;
-  static unsigned long nextRadioInfoTime = 0;
-  
+
   // some internal static values for parsing the input
-  static char command;
-  static int16_t value;
-  static RADIO_FREQ lastf = 0;
+  static RADIO_FREQ lastFrequency = 0;
   RADIO_FREQ f = 0;
-  
-  char c;
 
   KEYSTATE k = getLCDKeypadKey();
 
@@ -272,24 +261,36 @@ void loop() {
 
   } else if (k == KEYSTATE_UP) {
     // increase volume
-    Serial.println("increase volume");
     int v = radio.getVolume();
-    if (v < 15) radio.setVolume(++v);
+    if (v < radio.getMaxVolume()) radio.setVolume(++v);
+    lcd.setCursor(0, 1);
+    lcd.print("vol:");
+    lcd.print(v);
+    nextClearTime = now + 2000;
 
   } else if (k == KEYSTATE_DOWN) {
     // decrease volume
     int v = radio.getVolume();
     if (v > 0) radio.setVolume(--v);
+    lcd.setCursor(0, 1);
+    lcd.print("vol:");
+    lcd.print(v);
+    nextClearTime = now + 2000;
 
   } else if (k == KEYSTATE_LEFT) {
     radio.seekDown(true);
 
   } else if (k == KEYSTATE_SELECT) {
     // 10110
-    radio.setFrequency(10110);
+    if (presetIndex < (sizeof(preset) / sizeof(RADIO_FREQ)) - 1) {
+      presetIndex++;
+    } else {
+      presetIndex = 0;
+    }
+    radio.setFrequency(preset[presetIndex]);
 
   } else {
-    // 
+    //
   }
 
   // check for RDS data
@@ -298,14 +299,21 @@ void loop() {
   // update the display from time to time
   if (now > nextFreqTime) {
     f = radio.getFrequency();
-    if (f != lastf) {
+    if (f != lastFrequency) {
       // print current tuned frequency
-      DisplayFrequency(f);
-      lastf = f;
-    } // if
+      DisplayFrequency();
+      lastFrequency = f;
+    }  // if
     nextFreqTime = now + 400;
-  } // if  
+  }  // if
 
-} // loop
+  // clear 2. line of display
+  if ((nextClearTime) && (now > nextClearTime)) {
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    nextClearTime = 0;
+  }  // if
+
+}  // loop
 
 // End.
